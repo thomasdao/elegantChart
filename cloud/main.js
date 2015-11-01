97d11f9ac32d1a0b49aa515c437239e0c28faf41
thomasdao <thomas.daoxuandung@gmail.com>
2015-11-01 14:39:49 +0800

    Add validation to prevent spam

--- cloud/main.js
+++ cloud/main.js
@@ -4,6 +4,11 @@ var validator = require('cloud/libs/validator.min.js');
 
 Parse.Cloud.beforeSave("Subscriber", function(request, response) {
   var email = request.object.get("email");
+  var name = request.object.get("name");
+  if (name && name.indexOf("http") != -1) {
+  	response.error("Invalid name");
+  }
+  
   if (email && validator.isEmail(email)) {
     response.success();
   } else {
